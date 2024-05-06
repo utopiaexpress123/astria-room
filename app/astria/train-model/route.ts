d75458dc3404f2b789c2c5dfad80b1d6e1fd51e2
rick-sys <ricksanchezy2x@gmail.com>
2024-05-06 19:07:44 +0200

    ff

--- app/astria/train-model/route.ts
+++ app/astria/train-model/route.ts
@@ -138,7 +138,7 @@ export async function POST(request: Request) {
         callback: trainWenhookWithParams,
         prompts_attributes: [
           {
-            text: `Swedish style <lora:epi_noiseoffset2:0.5><lora:FilmVelvia2:0.5><lora:add_detail:0.5><lora:epiCRealismHelper:0.2> --mask_prompt windows door --mask_invert --controlnets mlsd segroom --controlnet_weights 0.5 1`,
+            text: `Clean scandinavian design living room, natural light, architecture magazine cover <lora:epi_noiseoffset2:0.5><lora:FilmVelvia2:0.5><lora:add_detail:0.5><lora:epiCRealismHelper:0.2> --mask_prompt windows door --mask_invert --controlnets mlsd segroom --controlnet_weights 0.5 1`,
             callback: promptWebhookWithParams,
             negative_prompt:`painted,sketch,deformed,easynegative`,
             num_images: 4,
