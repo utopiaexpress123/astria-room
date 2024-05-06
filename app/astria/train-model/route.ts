4d8067178d5217e7ec887204dd1722c66e0ab85a
rick-sys <ricksanchezy2x@gmail.com>
2024-05-07 00:44:03 +0200

    bnbb

--- app/astria/train-model/route.ts
+++ app/astria/train-model/route.ts
@@ -50,10 +50,10 @@ export async function POST(request: Request) {
     );
   }
 
-  if (images?.length < 4) {
+  if (images?.length < 1) {
     return NextResponse.json(
       {
-        message: "Upload at least 4 sample images",
+        message: "Upload at least 1 sample images",
       },
       { status: 500 }
     );
