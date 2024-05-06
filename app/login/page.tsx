e0aa6a194a08f73860705fb7e968babb8e938540
rick-sys <ricksanchezy2x@gmail.com>
2024-05-07 00:06:50 +0200

    dfdf

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,6 +4,13 @@ import { redirect } from "next/navigation";
 import { Database } from "../../types/supabase";
 import { Login } from "./components/Login";
 
+import { createClient } from '@supabase/supabase-js';
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+
+const supabase = createClient('https://gjaxlmikqiidtokctpmh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqYXhsbWlrcWlpZHRva2N0cG1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ4OTUxODUsImV4cCI6MjAzMDQ3MTE4NX0.EETffY08sBXZErlomb5ERr5Ofk3CUIA7OcW2MfYINWQ')
+
+
 export const dynamic = "force-dynamic";
 
 export default async function LoginPage({
@@ -27,6 +34,11 @@ export default async function LoginPage({
   return (
     <div className="flex flex-col flex-1 w-full h-[calc(100vh-73px)]">
       <Login host={host} searchParams={searchParams} />
+  <Auth
+    supabaseClient={supabase}
+    appearance={{ theme: ThemeSupa }}
+    providers={['google']}
+  />
     </div>
   );
 }
