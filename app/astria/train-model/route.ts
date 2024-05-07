6ffceeedfa68c519e31bae806791d427c3b85aec
rick-sys <ricksanchezy2x@gmail.com>
2024-05-07 15:23:30 +0200

    rr

--- app/astria/train-model/route.ts
+++ app/astria/train-model/route.ts
@@ -131,11 +131,11 @@ export async function POST(request: Request) {
         title: name,
         // Hard coded tune id of Realistic Vision v5.1 from the gallery - https://www.astria.ai/gallery/tunes
         // https://www.astria.ai/gallery/tunes/690204/prompts
-        base_tune_id: 690204,
+        base_tune_id: 666678,
         name: type,
         branch: astriaTestModeIsOn ? "fast" : "sd15",
         token: "ohwx",
-        input_image_url: images,
+        image_urls: images,
         callback: trainWenhookWithParams,
         prompts_attributes: [
           {
