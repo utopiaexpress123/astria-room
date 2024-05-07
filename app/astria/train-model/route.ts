b7a6eb64b8b222bd23b2836b080ffa144bf37d8f
rick-sys <ricksanchezy2x@gmail.com>
2024-05-07 16:24:34 +0200

    Update route.ts

--- app/astria/train-model/route.ts
+++ app/astria/train-model/route.ts
@@ -131,7 +131,7 @@ export async function POST(request: Request) {
         title: name,
         // Hard coded tune id of Realistic Vision v5.1 from the gallery - https://www.astria.ai/gallery/tunes
         // https://www.astria.ai/gallery/tunes/690204/prompts
-        base_tune_id: 690204,
+        base_tune_id: 666678,
         name: type,
         branch: astriaTestModeIsOn ? "fast" : "sd15",
         image_urls: images,
@@ -147,7 +147,7 @@ export async function POST(request: Request) {
       },
     };
 
-    const response = await axios.post(DOMAIN + "/tunes", body, {
+    const response = await axios.post(DOMAIN + "/tunes/666678/prompts", body, {
       headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${API_KEY}`,
