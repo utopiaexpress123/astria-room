b21ccf18ef3ec22662f2d56f687489d917cb233d
rick-sys <ricksanchezy2x@gmail.com>
2024-05-07 16:20:16 +0200

    f

--- app/astria/train-model/route.ts
+++ app/astria/train-model/route.ts
@@ -131,15 +131,15 @@ export async function POST(request: Request) {
         title: name,
         // Hard coded tune id of Realistic Vision v5.1 from the gallery - https://www.astria.ai/gallery/tunes
         // https://www.astria.ai/gallery/tunes/690204/prompts
-        base_tune_id: 657379,
+        base_tune_id: 690204,
         name: type,
         branch: astriaTestModeIsOn ? "fast" : "sd15",
         image_urls: images,
         callback: trainWenhookWithParams,
         prompts_attributes: [
           {
-            text: "Clean scandinavian design living room, natural light, architecture magazine cover ",
-            negative_prompt:"painted, sketch, deformed, easynegative",
+            text: `Clean scandinavian design living room, natural light, architecture magazine cover`,
+            negative_prompt:``,
             callback: promptWebhookWithParams,
             num_images: 2,
           },
